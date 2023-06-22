08e148da35f2e05f15e7ca718c5436380558f9a5
chaerlo127 <monynony0203@gmail.com>
2023-06-22 20:57:49 +0900

    feat: first commit

---
+++ src/main/kotlin/com/psr/psr/PsrApplication.kt
@@ -0,0 +1,11 @@
+package com.psr.psr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PsrApplication
+
+fun main(args: Array<String>) {
+	runApplication<PsrApplication>(*args)
+}
---
+++ src/test/kotlin/com/psr/psr/PsrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.psr.psr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PsrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
