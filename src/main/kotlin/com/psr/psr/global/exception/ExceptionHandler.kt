16da422abc98d716598535b135c3b95300e519e1
sojungpp <sophie2458@naver.com>
2023-07-14 01:35:57 +0900

    #1 feat: exception handler 새성

---
+++ src/main/kotlin/com/psr/psr/global/exception/ExceptionHandler.kt
@@ -0,0 +1,15 @@
+package com.psr.psr.global.exception
+
+import com.psr.psr.global.dto.BaseResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandler {
+    @ExceptionHandler(BaseException::class)
+    protected fun handleBaseException(e: BaseException): ResponseEntity<BaseRes>{
+        return ResponseEntity.status(e.baseResponseCode.status)
+            .body(BaseRes(e.baseResponseCode.status, e.baseResponseCode.message))
+    }
+}
