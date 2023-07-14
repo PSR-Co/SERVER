8ca0412e66473628e891b33130706a61a98d4e40
psyeon1120 <psycsh@naver.com>
2023-07-14 23:33:39 +0900

    #3 docs: nohup 로그 삭제

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,12 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할 애플리케이션이 없습니다." >> $REPOSITORY/deploy.log
+  echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID" >> $REPOSITORY/deploy.log
+  echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH " >> $REPOSITORY/deploy.log
-nohup java -jar $JAR_PATH
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
