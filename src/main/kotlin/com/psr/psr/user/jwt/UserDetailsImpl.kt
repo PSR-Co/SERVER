f8a3a0659d1b825e138ad62b055b5c2ff6f01dc3
chaerlo127 <monynony0203@gmail.com>
2023-07-18 00:18:10 +0900

    #7 feat: UserDetails 코드 추가

---
+++ src/main/kotlin/com/psr/psr/user/jwt/UserDetailsImpl.kt
@@ -0,0 +1,28 @@
+package com.psr.psr.user.jwt
+
+import com.psr.psr.user.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class UserDetailsImpl(val user:User) :UserDetails {
+
+    var enabled: Boolean = true
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        // todo: user role entity 생기면 넣어두기
+        val grantedAuthority = SimpleGrantedAuthority("user.role.name")
+        return mutableListOf(grantedAuthority)
+    }
+
+    override fun getPassword(): String = user.password
+
+    override fun getUsername(): String = user.id.toString()
+
+    override fun isAccountNonExpired(): Boolean = enabled
+
+    override fun isAccountNonLocked(): Boolean = enabled
+
+    override fun isCredentialsNonExpired(): Boolean = enabled
+
+    override fun isEnabled(): Boolean = enabled
+}
---
+++ src/main/kotlin/com/psr/psr/user/jwt/UserDetailsServiceImpl.kt
@@ -0,0 +1,20 @@
+package com.psr.psr.user.jwt
+
+import com.psr.psr.user.entity.User
+import com.psr.psr.user.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UserDetailsServiceImpl(private val userRepository: UserRepository) :UserDetailsService {
+
+
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val user:User = userRepository.findByIdOrNull(username?.toLong() ?: 0L) ?: throw UsernameNotFoundException("사용자 id를 찾을 수 없습니다.")
+        return UserDetailsImpl(user)
+    }
+}
