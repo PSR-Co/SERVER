feb1222e212dee7820bc92870dfdde73ece7f997
sojungpp <sophie2458@naver.com>
2023-07-23 21:59:56 +0900

    #1 fix: reportCategory 종류 수정

--- src/main/kotlin/com/psr/psr/product/entity/ReportCategory.kt
+++ src/main/kotlin/com/psr/psr/product/entity/ReportCategory.kt
@@ -1,8 +1,9 @@
 package com.psr.psr.product.entity
 
 enum class ReportCategory(val value: String) {
-    JUNK("홍보/도배"),
+    JUNK("스팸홍보/도배"),
+    ABUSE("욕설/혐오/차별"),
     PORN("음란물/유해한 정보"),
-    POOR_CONTENT("내용이 부실함"),
+    FRAUD("사기/불법정보"),
     NOT_FIT("게시글 성격에 부적합함")
 }
