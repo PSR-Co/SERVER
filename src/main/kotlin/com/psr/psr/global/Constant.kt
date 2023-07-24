fece04eddaaff30586fcbb020c521f545e5c87dc
chaerlo127 <monynony0203@gmail.com>
2023-07-24 19:31:48 +0900

    #9 chore: JWT Constant type 변경

--- src/main/kotlin/com/psr/psr/global/Constant.kt
+++ src/main/kotlin/com/psr/psr/global/Constant.kt
@@ -2,8 +2,8 @@ package com.psr.psr.global
 
 class Constant {
     companion object JWT{
-        var AUTHORIZATION_HEADER = "Authorization"
-        var BEARER_PREFIX: String = "Bearer "
+        const val AUTHORIZATION_HEADER = "Authorization"
+        const val BEARER_PREFIX: String = "Bearer "
     }
 
 }
