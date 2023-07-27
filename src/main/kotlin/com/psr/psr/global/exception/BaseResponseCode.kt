78eb30c651541b0563123ea278ba6980e150b39f
psyeon1120 <psycsh@naver.com>
2023-07-28 02:32:07 +0900

    #16 feat: 문의하기 등록 API 생성

--- src/main/kotlin/com/psr/psr/global/exception/BaseResponseCode.kt
+++ src/main/kotlin/com/psr/psr/global/exception/BaseResponseCode.kt
@@ -16,6 +16,7 @@ enum class BaseResponseCode(status: HttpStatus, message: String) {
     EXISTS_PHONE(HttpStatus.BAD_REQUEST, "이미 가입되어 있는 휴대폰 번호입니다."),
     EXISTS_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입되어 있는 이메일입니다."),
     EXISTS_NICKNAME(HttpStatus.BAD_REQUEST, "이미 가입되어 있는 닉네임입니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
     // User - type
     INVALID_USER_TYPE_NAME(HttpStatus.BAD_REQUEST, "올바르지 않은 사용자 역할입니다."),
--- src/main/kotlin/com/psr/psr/inquiry/controller/InquiryController.kt
+++ src/main/kotlin/com/psr/psr/inquiry/controller/InquiryController.kt
@@ -1,12 +1,19 @@
 package com.psr.psr.inquiry.controller
 
+import com.psr.psr.global.dto.BaseResponse
+import com.psr.psr.inquiry.dto.InquiryReq
 import com.psr.psr.inquiry.service.InquiryService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/inquiries")
 class InquiryController(
         private val inquiryService: InquiryService
 ){
+        // 문의하기 등록
+        @PostMapping
+        fun makeInquiry (@RequestBody @Valid inquiryReq: InquiryReq) : BaseResponse<Unit> {
+                return BaseResponse(inquiryService.makeInquiry(inquiryReq, 1L))
+        }
 }
