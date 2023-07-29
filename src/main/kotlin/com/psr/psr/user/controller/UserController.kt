7428042952194031d4686763d4e30aa800951810
chaerlo127 <monynony0203@gmail.com>
2023-07-29 17:53:51 +0900

    #21 fix: CheckNicknameReq 이를 변경

--- src/main/kotlin/com/psr/psr/user/controller/UserController.kt
+++ src/main/kotlin/com/psr/psr/user/controller/UserController.kt
@@ -4,7 +4,7 @@ import com.psr.psr.global.dto.BaseResponse
 import com.psr.psr.global.jwt.dto.TokenRes
 import com.psr.psr.user.dto.LoginReq
 import com.psr.psr.user.dto.SignUpReq
-import com.psr.psr.user.dto.checkNicknameReq
+import com.psr.psr.user.dto.CheckNicknameReq
 import com.psr.psr.user.service.UserService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -37,7 +37,7 @@ class UserController(
          */
         @PostMapping("/nickname")
         @ResponseBody
-        fun checkDuplicateNickname (@RequestBody @Validated nicknameReq: checkNicknameReq) : BaseResponse<Boolean>{
+        fun checkDuplicateNickname (@RequestBody @Validated nicknameReq: CheckNicknameReq) : BaseResponse<Boolean>{
                 // 사용 가능 : True, 사용 불가 : False
                 return BaseResponse(!userService.checkDuplicateNickname(nicknameReq.nickname))
         }
