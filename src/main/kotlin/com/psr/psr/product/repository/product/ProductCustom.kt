626c0b33cc04f8c754633367bcfd2438cbadab90
sojungpp <sophie2458@naver.com>
2023-08-01 00:37:25 +0900

    #13 feat: product QueryDSL 설정 추가

--- src/main/kotlin/com/psr/psr/product/repository/product/ProductCustom.kt
+++ src/main/kotlin/com/psr/psr/product/repository/product/ProductCustom.kt
@@ -1,4 +1,11 @@
 package com.psr.psr.product.repository.product
 
+import com.psr.psr.product.dto.response.PopularProductDetail
+import com.psr.psr.product.dto.response.ProductDetail
+import com.psr.psr.user.entity.Category
+import com.psr.psr.user.entity.User
+
 interface ProductCustom {
+    fun findTop5PopularProducts(user: User, category: List<Category>): List<PopularProductDetail>
+    fun findAllCategoryProducts(user: User, category: List<Category>): List<ProductDetail>
 }
--- src/main/kotlin/com/psr/psr/product/repository/product/ProductRepository.kt
+++ src/main/kotlin/com/psr/psr/product/repository/product/ProductRepository.kt
@@ -1,9 +1,11 @@
 package com.psr.psr.product.repository.product
 
+
 import com.psr.psr.product.entity.product.Product
+
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductRepository: JpaRepository<Product, Long> {
+interface ProductRepository: JpaRepository<Product, Long>, ProductCustom {
 }
