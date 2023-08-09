e5c941325ef5460b923043cb1a66cf6a070a3470
sojungpp <sophie2458@naver.com>
2023-08-09 18:24:30 +0900

    #90 feat: 찜 목록 API 추가

--- src/main/kotlin/com/psr/psr/product/dto/assembler/ProductAssembler.kt
+++ src/main/kotlin/com/psr/psr/product/dto/assembler/ProductAssembler.kt
@@ -1,16 +1,31 @@
 package com.psr.psr.product.dto.assembler
 
+import com.psr.psr.product.dto.response.GetLikeProductsRes
 import com.psr.psr.product.dto.response.GetProductDetailRes
 import com.psr.psr.product.dto.response.GetProductsByUserRes
 import com.psr.psr.product.dto.response.MyProduct
 import com.psr.psr.product.entity.Product
 import com.psr.psr.product.entity.ProductImg
+import com.psr.psr.product.entity.ProductLike
 import com.psr.psr.user.entity.User
 import org.springframework.stereotype.Component
 
 @Component
 class ProductAssembler {
     fun toMyProductDto(product: Product, imgUrl: String): MyProduct {
+        return MyProduct(
+            productId = product.id,
+            imgUrl = imgUrl,
+            category = product.category.value,
+            name = product.name,
+            price = product.price
+        )
+    }
+
+    fun toMyProductDto(product: Product): MyProduct {
+        val imgUrl =
+            if (product.imgs.isNotEmpty()) product.imgs[0].imgUrl
+            else null
         return MyProduct(
             productId = product.id,
             imgUrl = imgUrl,
@@ -41,6 +56,13 @@ class ProductAssembler {
             price = product.price,
             description = product.description,
             isLike = isLike
+        )
+
+    }
+
+    fun toGetLikeProductsRes(productLikeList: List<ProductLike>?): GetLikeProductsRes {
+        return GetLikeProductsRes(
+            productList = productLikeList?.map { pl -> this.toMyProductDto(pl.product) }?.toList()
         )
 
     }
--- src/main/kotlin/com/psr/psr/product/dto/response/MyProduct.kt
+++ src/main/kotlin/com/psr/psr/product/dto/response/MyProduct.kt
@@ -2,7 +2,7 @@ package com.psr.psr.product.dto.response
 
 data class MyProduct(
     val productId: Long,
-    val imgUrl: String,
+    val imgUrl: String?,
     val category: String,
     val name: String,
     val price: Int
