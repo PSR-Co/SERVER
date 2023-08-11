aaa3611d7d739373e775ffbd54203c17d05459d2
sojungpp <sophie2458@naver.com>
2023-08-11 23:12:04 +0900

    #120 feat: 상품 등록 API

--- src/main/kotlin/com/psr/psr/product/dto/assembler/ProductAssembler.kt
+++ src/main/kotlin/com/psr/psr/product/dto/assembler/ProductAssembler.kt
@@ -1,10 +1,12 @@
 package com.psr.psr.product.dto.assembler
 
 import com.psr.psr.global.entity.ReportCategory
+import com.psr.psr.product.dto.request.CreateproductReq
 import com.psr.psr.product.dto.response.*
 import com.psr.psr.product.entity.Product
 import com.psr.psr.product.entity.ProductImg
 import com.psr.psr.product.entity.ProductReport
+import com.psr.psr.user.entity.Category
 import com.psr.psr.user.entity.User
 import org.springframework.data.domain.Page
 import org.springframework.stereotype.Component
@@ -107,5 +109,21 @@ class ProductAssembler {
         )
     }
 
+    fun toProductEntity(user: User, request: CreateproductReq): Product {
+        return Product(
+            user = user,
+            name = request.name,
+            category = Category.getCategoryByValue(request.category),
+            price = request.price,
+            description = request.description
+        )
+    }
+
+    fun toProductImgEntity(product: Product, imgUrl: String): ProductImg {
+        return ProductImg(
+            product = product,
+            imgUrl = imgUrl
+        )
+    }
 
 }
