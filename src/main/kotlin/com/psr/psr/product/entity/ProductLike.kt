d062aba435f3a67d0c5acd19cd146354d650f30a
sojungpp <sophie2458@naver.com>
2023-08-12 21:43:18 +0900

    #124 feat: Product 관련 cascade 추가

--- src/main/kotlin/com/psr/psr/product/entity/ProductLike.kt
+++ src/main/kotlin/com/psr/psr/product/entity/ProductLike.kt
@@ -3,8 +3,10 @@ package com.psr.psr.product.entity
 import com.psr.psr.global.entity.BaseEntity
 import com.psr.psr.user.entity.User
 import jakarta.persistence.*
+import org.hibernate.annotations.SQLDelete
 
 @Entity
+@SQLDelete(sql = "UPDATE product_like SET status = 'inactive', updated_at = CURRENT_TIMESTAMP WHERE id = ?")
 data class ProductLike(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
