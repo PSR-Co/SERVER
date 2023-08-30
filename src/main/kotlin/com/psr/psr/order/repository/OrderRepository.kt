a71803ca10a803d92d72790beb2468deceded602
psyeon1120 <psycsh@naver.com>
2023-08-31 01:49:25 +0900

    #161 hotfix: 잘못된 jpa 쿼리 -> queryDSL로 변경

--- src/main/kotlin/com/psr/psr/order/repository/OrderRepository.kt
+++ src/main/kotlin/com/psr/psr/order/repository/OrderRepository.kt
@@ -7,15 +7,13 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.time.LocalDate
 
 @Repository
-interface OrderRepository: JpaRepository<Order, Long> {
+interface OrderRepository: JpaRepository<Order, Long>, OrderCustom {
     fun findByIdAndStatus(orderId: Long, status: String): Order?
     fun findByUserAndOrderStatusAndStatus(orderer: User, orderStatus: OrderStatus, status: String, pageable: Pageable): Page<Order>
     fun findByProductUserAndOrderStatusAndStatus(seller: User, orderStatus: OrderStatus, status: String, pageable: Pageable): Page<Order>
     fun findByUserAndStatus(orderer: User, status: String, pageable: Pageable): Page<Order>
     fun findByProductUserAndStatus(seller: User, status: String, pageable: Pageable): Page<Order>
     fun deleteByUser(user: User)
-    fun findByCreatedAt_DateAndOrderStatusAndStatus(date: LocalDate, orderStatus: OrderStatus, status: String): List<Order>
 }
