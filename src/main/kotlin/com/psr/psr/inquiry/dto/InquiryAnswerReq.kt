8ee176523c32c6517fc93634cbeb404375af47ed
psyeon1120 <psycsh@naver.com>
2023-11-10 14:33:20 +0900

    #188 docs: 문의하기 Swagger 추가

--- src/main/kotlin/com/psr/psr/inquiry/dto/InquiryAnswerReq.kt
+++ src/main/kotlin/com/psr/psr/inquiry/dto/InquiryAnswerReq.kt
@@ -1,9 +1,11 @@
 package com.psr.psr.inquiry.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
 data class InquiryAnswerReq (
+    @Schema(description = "답변", example = "답변 내용")
     @field:NotBlank(message = "답변을 입력해주세요.")
     @field:Size(max = 250, message = "답변은 최대 250자입니다.")
     val answer: String
