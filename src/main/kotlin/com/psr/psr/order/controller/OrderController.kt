7126152f40b8be79fe156283d6202929c1efeab0
psyeon1120 <psycsh@naver.com>
2023-11-10 23:34:49 +0900

    #188 docs: 요청 Swagger 추가

--- src/main/kotlin/com/psr/psr/order/controller/OrderController.kt
+++ src/main/kotlin/com/psr/psr/order/controller/OrderController.kt
@@ -11,9 +11,18 @@ import com.psr.psr.order.dto.OrderListRes
 import com.psr.psr.order.dto.OrderReq
 import com.psr.psr.order.dto.OrderRes
 import com.psr.psr.order.service.OrderService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import org.springdoc.core.annotations.ParameterObject
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -29,6 +38,27 @@ class OrderController(
     private val orderService: OrderService
 ) {
     // 요청하기
+    @Operation(summary = "요청하기(박서연)", description = "상품에 대한 요청을 한다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            ApiResponse(
+                responseCode = "400",
+                description = "이름을 입력해주세요.<br>" +
+                        "이름은 최대 100자입니다.<br>" +
+                        "문의사항을 입력해주세요.<br>" +
+                        "문의사항은 최대 250자입니다.<br>" +
+                        "요청 상세설명을 입력해주세요.<br>" +
+                        "요청 상세설명은 최대 250자입니다.<br>" +
+                        "상품ID를 입력해주세요.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "해당 상품을 찾을 수 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            )]
+    )
     @PostMapping
     fun makeOrder(
         @AuthenticationPrincipal userAccount: UserAccount,
@@ -41,21 +71,59 @@ class OrderController(
     }
 
     // 요청 상세 조회
+    @Operation(summary = "요청 상세 조회(박서연)", description = "요청을 상세조회 한다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            ApiResponse(
+                responseCode = "403",
+                description = "권한이 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "해당 요청을 찾을 수 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            )]
+    )
     @GetMapping("/{orderId}")
     fun getOrderDetail(
         @AuthenticationPrincipal userAccount: UserAccount,
-        @PathVariable orderId: Long
+        @Parameter(description = "(Long) 요청 Id", example = "1") @PathVariable orderId: Long
     ): BaseResponse<OrderRes> {
         return BaseResponse(orderService.getOrderDetail(userAccount.getUser(), orderId))
     }
 
     // 요청 목록 조회
+    @Operation(summary = "요청 목록 조회(박서연)", description = "요청 목록을 조회 한다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            ApiResponse(
+                responseCode = "400",
+                description = "올바르지 않은 요청 타입입니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "권한이 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "해당 요청을 찾을 수 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            )]
+    )
     @GetMapping
     fun getOrderList(
         @AuthenticationPrincipal userAccount: UserAccount,
-        @RequestParam type: String,
-        @RequestParam(required = false) status: String?,
-        @PageableDefault(size = 10, sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable
+        @Parameter(description = "요청 타입", example = "sell/order") @RequestParam type: String,
+        @Parameter(
+            description = "요청 상태",
+            example = "요청대기/진행중/진행완료/요청취소"
+        ) @RequestParam(required = false) status: String?,
+        @ParameterObject @PageableDefault(size = 10, sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable
     ): BaseResponse<Page<OrderListRes>> {
         if (type !in listOf(SELL, ORDER)) return BaseResponse(BaseResponseCode.INVALID_ORDER_TYPE)
 
@@ -66,10 +134,35 @@ class OrderController(
     }
 
     // 요청 수정
+    @Operation(summary = "요청 수정(박서연)", description = "요청을 수정한다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            ApiResponse(
+                responseCode = "400",
+                description = "이름을 입력해주세요.<br>" +
+                        "이름은 최대 100자입니다.<br>" +
+                        "문의사항을 입력해주세요.<br>" +
+                        "문의사항은 최대 250자입니다.<br>" +
+                        "요청 상세설명을 입력해주세요.<br>" +
+                        "요청 상세설명은 최대 250자입니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "권한이 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "해당 요청을 찾을 수 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            )]
+    )
     @PatchMapping("/{orderId}")
     fun editOrder(
         @AuthenticationPrincipal userAccount: UserAccount,
-        @PathVariable orderId: Long,
+        @Parameter(description = "(Long) 요청 Id", example = "1") @PathVariable orderId: Long,
         @RequestBody @Valid orderReq: OrderReq
     ): BaseResponse<Unit> {
         if (orderReq.websiteUrl.isNullOrBlank()) orderReq.websiteUrl = null
@@ -77,11 +170,42 @@ class OrderController(
     }
 
     // 요청 상태 수정
+    @Operation(summary = "요청 상태 수정(박서연)", description = "요청 상태를 수정한다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "요청에 성공했습니다."),
+            ApiResponse(
+                responseCode = "400",
+                description = "요청 상태를 입력해주세요.<br>" +
+                        "올바르지 않은 요청 상태입니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "권한이 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "해당 요청을 찾을 수 없습니다.",
+                content = arrayOf(Content(schema = Schema(implementation = BaseResponse::class)))
+            )]
+    )
     @PatchMapping("/{orderId}/status")
     fun editOrderStatus(
         @AuthenticationPrincipal userAccount: UserAccount,
-        @PathVariable orderId: Long,
-        @RequestBody status: Map<String, String>
+        @Parameter(description = "(Long) 요청 Id", example = "1") @PathVariable orderId: Long,
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "요청 상태",
+            content = [Content(
+                examples = [ExampleObject(
+                    value = "{" +
+                            "\"status\" : \"진행중\"" +
+                            "}"
+                )], schema = Schema(title = "status", allowableValues = ["진행중", "진행완료", "요청취소"])
+            )]
+
+        ) @RequestBody status: Map<String, String>
     ): BaseResponse<Unit> {
         status[Constant.Order.STATUS] ?: return BaseResponse(BaseResponseCode.NULL_ORDER_STATUS)
         return BaseResponse(orderService.editOrderStatus(userAccount.getUser(), status[STATUS]!!, orderId))
--- src/main/kotlin/com/psr/psr/order/dto/OrderReq.kt
+++ src/main/kotlin/com/psr/psr/order/dto/OrderReq.kt
@@ -1,22 +1,28 @@
 package com.psr.psr.order.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
 data class OrderReq (
+    @Schema(description = "상품 Id", example = "1")
     val productId: Long?,
 
+    @Schema(description = "요청자 이름", example = "박서연")
     @field:NotBlank(message = "이름을 입력해주세요.")
     @field:Size(max = 100, message = "이름은 최대 100자입니다.")
     val ordererName: String,
 
+    @Schema(description = "사업자/쇼핑몰 URL", example = "www.123.com")
     @field:Size(max = 255)
     var websiteUrl: String? = null,
 
+    @Schema(description = "문의사항", example = "문의사항 내용")
     @field:NotBlank(message = "문의사항을 입력해주세요.")
     @field:Size(max = 250, message = "문의사항은 최대 250자입니다.")
     val inquiry: String,
 
+    @Schema(description = "요청 상세 설명", example = "오늘 해주세요")
     @field:NotBlank(message = "요청 상세설명을 입력해주세요.")
     @field:Size(max = 250, message = "요청 상세설명은 최대 250자입니다.")
     val description: String
